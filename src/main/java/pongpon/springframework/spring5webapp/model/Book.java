35ebced45fc97859391d53c30b593f2169141da0
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-10-20 01:02:55 -0500

    Inititialize data using ApplicationListener

--- src/main/java/pongpon/springframework/spring5webapp/model/Book.java
+++ src/main/java/pongpon/springframework/spring5webapp/model/Book.java
@@ -67,6 +67,14 @@ public class Book {
         this.publisher = publisher;
     }
 
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
