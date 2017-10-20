f818a1da2f9b424979ab5dfe5b50a64abee164d4
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-10-20 09:33:51 -0500

    Add new model: Publisher and form a one-to-one relationship with Book

--- src/main/java/pongpon/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/pongpon/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -5,18 +5,22 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import pongpon.springframework.spring5webapp.model.Author;
 import pongpon.springframework.spring5webapp.model.Book;
+import pongpon.springframework.spring5webapp.model.Publisher;
 import pongpon.springframework.spring5webapp.repositories.AuthorRepository;
 import pongpon.springframework.spring5webapp.repositories.BookRepository;
+import pongpon.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,8 +30,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     public void initData() {
 
+        Publisher happerCollins = new Publisher("Harper Collins", "911 South Locust");
+        publisherRepository.save(happerCollins);
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", happerCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -35,8 +42,9 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development with EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development with EJB", "23444", happerCollins);
         rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
--- src/main/java/pongpon/springframework/spring5webapp/model/Book.java
+++ src/main/java/pongpon/springframework/spring5webapp/model/Book.java
@@ -12,7 +12,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -22,13 +24,13 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -59,20 +61,20 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
     public Set<Author> getAuthors() {
         return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
@@ -96,7 +98,7 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
+                ", publisher=" + publisher +
                 ", authors=" + authors +
                 '}';
     }
