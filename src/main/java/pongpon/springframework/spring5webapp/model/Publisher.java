caeea7379311c11d0e68fe561f3293b07a0b11b0
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-10-22 00:56:56 -0500

    Add views using Themyleaf

--- src/main/java/pongpon/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/pongpon/springframework/spring5webapp/model/Publisher.java
@@ -13,6 +13,9 @@ public class Publisher {
 
     private String address;
 
+    public Publisher() {
+    }
+
     public Publisher(String name, String address) {
         this.name = name;
         this.address = address;
